eabba7bd3a7338272d4660b0f7b50338035fde0d
Lucas <lucas9godoy@gmail.com>
2025-11-26 12:52:54 -0300

    aplicamos text-base a input: 16px para q ios no haga zoom al input

--- frontend/src/components/ui/InputChat.jsx
+++ frontend/src/components/ui/InputChat.jsx
@@ -2,7 +2,7 @@ export const InputChat = (props) => {
   return (
     <input
       {...props}
-      className='w-full bg-transparent placeholder:text-slate-400 text-slate-700 text-sm border border-slate-200 rounded-md pl-10 pr-3 py-2 transition duration-300 ease focus:outline-none focus:border-slate-400 hover:border-slate-300 shadow-sm focus:shadow text-white'
+      className='w-full bg-transparent placeholder:text-slate-400 border border-slate-200 rounded-md pl-10 pr-3 py-2 transition duration-300 ease focus:outline-none focus:border-slate-400 hover:border-slate-300 shadow-sm focus:shadow text-white text-base'
     />
   );
 };
